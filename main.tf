dcbaa17d30e9bb5a55b684f6ca1302363395b2d1
Samir DERKAOUI <126152143+Kaiser016X@users.noreply.github.com>
2024-07-13 15:56:24 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,13 +5,12 @@ resource "tls_private_key" "generated" {
 
 resource "local_file" "private_key_pem" {
   content         = tls_private_key.generated.private_key_pem
-  filename        = "MyAWSKey.pem"
+  filename        = var.private_key_filename
   file_permission = "0600"
 }
 
-
 resource "aws_key_pair" "generated" {
-  key_name   = "MyAWSKey"
+  key_name   = var.key_name
   public_key = tls_private_key.generated.public_key_openssh
 
   lifecycle {
