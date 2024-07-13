f7edec9a1ea0543930d127f959f16432239d51ce
Samir DERKAOUI <126152143+Kaiser016X@users.noreply.github.com>
2024-07-13 15:53:29 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+resource "tls_private_key" "generated" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "private_key_pem" {
+  content         = tls_private_key.generated.private_key_pem
+  filename        = "MyAWSKey.pem"
+  file_permission = "0600"
+}
+
+
+resource "aws_key_pair" "generated" {
+  key_name   = "MyAWSKey"
+  public_key = tls_private_key.generated.public_key_openssh
+
+  lifecycle {
+    ignore_changes = [key_name]
+  }
+}
